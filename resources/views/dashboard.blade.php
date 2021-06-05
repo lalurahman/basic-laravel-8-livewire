c8a612b245b5f6a662a27f96283d221f16c9785d
Lalu Abdurrahman <lalurahmanms@gmail.com>
2021-06-05 17:36:27 +0800

    view create post

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,7 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="p-4 bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <textarea name=""  rows="2" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+                <livewire:create-post />
             </div>
         </div>
     </div>
---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,6 @@
+<div class="p-4">
+    <textarea name="body"  rows="2" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+    <div class="flex justify-end">
+        <button class="mt-1 px-4 py-2 bg-blue-600 text-white hover:bg-blue-500 rounded-md">Post</button>
+    </div>
+</div>
